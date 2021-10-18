2b6d8921624c4aef640a07fa50d8bf5fa95073b1
Zygimantas <brinkis.zygimantas@gmail.com>
2021-10-18 19:54:30 +0300

    Bandymas
    Bandymas

---
+++ 13 Pamoka Task 1 pakartojimas nr 2/Galimybes.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Pamoka_Task_1_pakartojimas_nr_2
+{
+    enum Galimybes
+    {
+        nieko,
+        vairuoti,
+        vairuotiirgerti
+
+    }
+}
---
+++ 13 Pamoka Task 1 pakartojimas nr 2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13_Pamoka_Task_1_pakartojimas_nr_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        public static void IsvestiRezultata(Galimybes galimybe)
+        {
+            switch (galimybe)
+            {
+                case Galimybes.nieko:
+                    Console.WriteLine("Asmuo negali vairuoti ir gerti");
+                    break;
+
+                case Galimybes.vairuoti:
+                    Console.WriteLine("Asmuo gali vairuoti, taciau gerti negali");
+                    break;
+
+                case Galimybes.vairuotiirgerti:
+                    Console.WriteLine("Asmuo gali vairutoi ir gerti");
+                    break;
+            }
+        }
+    }
+}
