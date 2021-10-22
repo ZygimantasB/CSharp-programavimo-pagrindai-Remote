d2f22a1de22ede866cd48a2b13a5df61a0854995
Zygimantas <brinkis.zygimantas@gmail.com>
2021-10-22 14:56:34 +0300

    Bandymas
    Bandymas

--- 14 Pamoka task 7 FUUUUN/Program.cs
+++ 14 Pamoka task 7 FUUUUN/Program.cs
@@ -11,6 +11,9 @@ namespace _14_Pamoka_task_7_FUUUUN
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Sveikti jei turite vartotoja prasome prisijungti, jei ne registruokite nauja vartotoja");
+            string registracija_ar_prisijungimas = Console.ReadLine().ToLower();
+
             Console.WriteLine("Irasykite vartotojo varda");
             string vartotojoVardas = Console.ReadLine();
 
@@ -19,7 +22,19 @@ namespace _14_Pamoka_task_7_FUUUUN
 
             Program metaDuomenys = new Program();
 
-            metaDuomenys.Prisijungti(vartotojoVardas, vartotojoSlaptazodis);
+            //metaDuomenys.Prisijungti(vartotojoVardas, vartotojoSlaptazodis);
+
+            metaDuomenys.Registruotis(vartotojoVardas, vartotojoSlaptazodis);
+
+            //switch (registracija_ar_prisijungimas)
+            //{
+            //    case "Prisijungti":
+            //        metaDuomenys.Prisijungti(vartotojoVardas, vartotojoSlaptazodis);
+            //        break;
+            //    case "Registracija":
+            //        metaDuomenys.Registruotis(vartotojoVardas, vartotojoSlaptazodis);
+            //        break;
+            //}
 
             Console.ReadKey();
         }
@@ -28,26 +43,36 @@ namespace _14_Pamoka_task_7_FUUUUN
         {
             string filePatch = @"G:\My Drive\C Sharp mokymai video\Paskaitos video\14 Pamoka\Task 6 Nr 2\mytest.txt";
 
-            //if (registracijosVardas) ???
+            string perskaitomasFailas = File.ReadAllLines(filePatch).ToString();
 
-            using ()
+            if (/*perskaitomasFailas == "Zygimantas" ||*/ registracijosVardas == "Zygimantas")
+            {
+                Console.WriteLine("Vartotojas tokiu vardu yra registruotas");
+            }
+            else if (/*perskaitomasFailas.Length < 7 || */registracijosSlaptazodis.Length < 7)
+            {
+                Console.WriteLine("Slaptazodis negali buti ilgesnis nei 6 skaitmenys");
+            }
+            else
+            {
+                Console.WriteLine($"Vartojo vardas: {registracijosVardas}");
+                Console.WriteLine($"Vartojo slaptazodis: {registracijosSlaptazodis}");
+                Console.WriteLine("Sveikinu Jus sukurete nauja paskytra");
 
-            Console.WriteLine("Sveikinu Jus sukurete nauja paskytra");
-            Console.WriteLine($"Vartojo vardas: {registracijosVardas}");
-            Console.WriteLine($"Vartojo slaptazodis: {registracijosSlaptazodis}");
+                List<string> registruotoVartotojoInfo = new List<string>();
+                registruotoVartotojoInfo.Add(registracijosVardas + "\n");
+                registruotoVartotojoInfo.Add(registracijosSlaptazodis + "\n");
 
+                foreach (var irasaiFaile in registruotoVartotojoInfo)
+                {
+                    Console.WriteLine(irasaiFaile);
+                    File.AppendAllText(filePatch, irasaiFaile);
+                }
+            }
         }
 
         public void Prisijungti (string prisijungimoVardas, string prisijungimoSlaptazodis)
         {
-            //Console.WriteLine("Irasykite vartotojo varda");
-            //prisijungimoVardas = Console.ReadLine();
-
-            //Console.WriteLine("Irasykite vartotojo varda");
-            //prisijungimoSlaptazodis = Console.ReadLine();
-
-            //bool ar_nori_kartoti = true;
-            
            
                 if (prisijungimoVardas == "Zygimantas" && prisijungimoSlaptazodis == "123456")
                 {
@@ -57,15 +82,6 @@ namespace _14_Pamoka_task_7_FUUUUN
                 {
                     Console.WriteLine("Jusu prisijungimas nesekmingas");
                 }
-
-            //ar_nori_kartoti = prisijungimoVardas == "Zygimantas" && prisijungimoSlaptazodis == "123456";
-                
-
-            //if (ar_nori_kartoti)
-            //{
-            //    Console.WriteLine(true);
-            //}
-
         }
     }
 }
